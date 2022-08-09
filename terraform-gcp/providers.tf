774d9f912c1ad846f9ca63d51b204869f9a4433d
Shri Sai Sadhana Natarajan <shrisais@users.noreply.github.com>
2022-08-08 20:05:38 -0700

    Added workflows for terraform

--- terraform-gcp/providers.tf
+++ terraform-gcp/providers.tf
@@ -1,6 +1,5 @@
 // Configure the Google Cloud provider
 provider "google" {
- credentials = "/Users/shri/Documents/Auth/cloud-native-ci-cd-358702-de450efe196d.json"
  project     = "${var.gcp_project}"
  region      = "${var.gcp_region}"
 }
